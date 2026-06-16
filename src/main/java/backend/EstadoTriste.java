61cb952e39c88b5d93906d636c8e8757fd88a0be
Isaac <isaac.cv123@gmail.com>
2026-06-15 21:24:36 -0400

    Creacion de la clase EstadoTriste el cual modifica las estadisticas de la mascota debido a baja felicidad

--- src/main/java/backend/EstadoTriste.java
+++ src/main/java/backend/EstadoTriste.java
@@ -1,4 +1,18 @@
 package backend;
 
-public class EstadoTriste {
+public class EstadoTriste implements EstadoMascota {
+    @Override
+    public void manejarEstado(Mascota mascota) {
+        mascota.setSalud(mascota.getSalud() - 1);
+    }
+
+    @Override
+    public String getEstado() {
+        return "Triste";
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "La mascota esta triste. Juega con ella para animarla";
+    }
 }
