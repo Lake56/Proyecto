8a03de7643d60cd4d17278de272f6dc5063fa4c4
Isaac <isaac.cv123@gmail.com>
2026-06-16 17:46:50 -0400

    Creacion de subclase Pez

---
+++ src/main/java/backend/Pez.java
@@ -0,0 +1,44 @@
+package backend;
+
+public class Pez extends Mascota{
+    public Pez(String nombre, float precio) {
+        super(nombre, precio);
+    }
+
+    @Override
+    public void alimentar() {
+        setHambre(getHambre() + 15);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void limpiar() {
+        setHigiene(getHigiene() + 50);
+        setSalud(getSalud() + 10);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void jugar() {
+        setFelicidad(getFelicidad()  + 10);
+
+        notificarObservers();
+    }
+
+    @Override
+    protected int getDegradacionHambre() {
+        return 4;
+    }
+
+    @Override
+    protected int getDegradacionFelicidad() {
+        return 2;
+    }
+
+    @Override
+    protected int getDegradacionHigiene() {
+        return 10;
+    }
+}
