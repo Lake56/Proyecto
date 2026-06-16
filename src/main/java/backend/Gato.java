ab883acff3ef4fa08342a63b4b6746284889f341
Isaac <isaac.cv123@gmail.com>
2026-06-16 17:43:10 -0400

    Creacion de subclase Gato

---
+++ src/main/java/backend/Gato.java
@@ -0,0 +1,45 @@
+package backend;
+
+public class Gato extends Mascota {
+    public Gato(String nombre, float precio) {
+        super(nombre, precio);
+    }
+
+    @Override
+    public void alimentar() {
+        setHambre(getHambre() + 20);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void limpiar() {
+        setHigiene(getHigiene() + 20);
+        setFelicidad(getFelicidad() - 15);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void jugar() {
+        setFelicidad(getFelicidad() + 20);
+        setHambre(getHambre() - 5);
+
+        notificarObservers();
+    }
+
+    @Override
+    protected int getDegradacionHambre() {
+        return 5;
+    }
+
+    @Override
+    protected int getDegradacionFelicidad() {
+        return 3;
+    }
+
+    @Override
+    protected int getDegradacionHigiene() {
+        return 2;
+    }
+}
