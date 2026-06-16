1576e6fe3da8702ac82d6519552bf098282ec6f4
Isaac <isaac.cv123@gmail.com>
2026-06-15 21:23:57 -0400

    Creacion de la clase EstadoEnfermo el cual modifica las estadisticas de la mascota

--- src/main/java/backend/EstadoEnfermo.java
+++ src/main/java/backend/EstadoEnfermo.java
@@ -1,4 +1,19 @@
 package backend;
 
-public class EstadoEnfermo {
+public class EstadoEnfermo implements EstadoMascota {
+    @Override
+    public void manejarEstado(Mascota mascota) {
+        mascota.setSalud(mascota.getSalud() - 3);
+        mascota.setFelicidad(mascota.getFelicidad() - 3);
+    }
+
+    @Override
+    public String getEstado() {
+        return "Enfermo";
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "La mascota esta enferma. Necesita atencion medica";
+    }
 }
