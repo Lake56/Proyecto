d726109c2642a7b65ce9f6b4f971b5a62b1aa978
Isaac <isaac.cv123@gmail.com>
2026-06-15 21:33:59 -0400

    Creacion de subclase Perro

--- src/main/java/backend/Perro.java
+++ src/main/java/backend/Perro.java
@@ -1,4 +1,54 @@
 package backend;
 
-public class Perro {
+public class Perro extends Mascota {
+    private String raza;
+
+    public Perro(String nombre, String raza, float precio) {
+        super(nombre, precio);
+        this.raza = raza;
+    }
+
+    @Override
+    public void alimentar() {
+        setHambre(getHambre() + 35);
+        setFelicidad(getFelicidad() + 5);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void limpiar() {
+        setHigiene(getHigiene() + 40);
+        setFelicidad(getFelicidad() - 7);
+
+        notificarObservers();
+    }
+
+    @Override
+    public void jugar() {
+        setFelicidad(getFelicidad() + 35);
+        setHambre(getHambre() - 15);
+        setHigiene(getHigiene() - 5);
+
+        notificarObservers();
+    }
+
+    @Override
+    protected int getDegradacionHambre() {
+        return 8;
+    }
+
+    @Override
+    protected int getDegradacionFelicidad() {
+        return 5;
+    }
+
+    @Override
+    protected int getDegradacionHigiene() {
+        return 6;
+    }
+
+    public String getRaza() {
+        return this.raza;
+    }
 }
