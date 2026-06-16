aa5b425f64e995a3d73090468d9a2620c6ccef28
Isaac <isaac.cv123@gmail.com>
2026-06-15 21:24:14 -0400

    Creacion de la clase EstadoCritico el cual modifica las estadisticas de la mascota

--- src/main/java/backend/EstadoCritico.java
+++ src/main/java/backend/EstadoCritico.java
@@ -1,4 +1,20 @@
 package backend;
 
-public class EstadoCritico {
+public class EstadoCritico implements EstadoMascota {
+    @Override
+    public void manejarEstado(Mascota mascota) {
+        mascota.setSalud(mascota.getSalud() - 7);
+        mascota.setFelicidad(mascota.getFelicidad() - 8);
+        mascota.setHambre(mascota.getHambre() - 5);
+    }
+
+    @Override
+    public String getEstado() {
+        return "Critico";
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "La mascota esta en estado critico. Debes actuar de inmediato";
+    }
 }
