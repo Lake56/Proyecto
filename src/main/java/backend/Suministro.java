329fa61761664834ac3f573997824893808470e4
Eltigre <bastiton@gmail.com>
2026-06-17 22:24:22 -0400

    Se crea la clase  Suministro, que extendera a medicamento y comida

---
+++ src/main/java/backend/Suministro.java
@@ -0,0 +1,11 @@
+package backend;
+
+public class Suministro{
+    private int Precio;
+    public Suministro(int Precio){
+        this.Precio=Precio;
+    }
+    public int getPrecio(){
+        return Precio;
+    }
+}
