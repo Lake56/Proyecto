5b321ff918a0f0c7fb57c6c531a25e4ae75fc0bb
Eltigre <bastiton@gmail.com>
2026-06-18 22:18:24 -0400

    Se crea el prototipo de la clase CLiente Virtual, aun con varios aspectos a corregir y mejorar

---
+++ src/main/java/backend/ClienteVirtual.java
@@ -0,0 +1,70 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.Random;
+/*
+    Prototipo Cliente virtual
+ */
+public class ClienteVirtual{
+    Random random= new Random(1);
+    Random random2= new Random(3);
+    Random random3= new Random(6);
+
+    private ArrayList<String> Mascotas;
+    private ArrayList<String> Nombres;
+    private int Dinero;
+    private String Interes;
+    private Mascota mascota;
+
+    public ClienteVirtual(int Dinero){
+
+        this.Dinero=Dinero;
+
+        Mascotas.add("perro");
+        Mascotas.add("Gato");
+        Mascotas.add("pez");
+        Mascotas.add("pajaro");
+
+        Nombres.add("Pinky");
+        Nombres.add("Atenea");
+        Nombres.add("Nieve");
+        Nombres.add("Doky");
+        Nombres.add("Calu");
+        Nombres.add("Ricky");
+
+        int rand1= random.nextInt();
+        int rand2= random2.nextInt();
+        int rand3= random3.nextInt();
+
+        if(rand1==0){
+            //se ocupara para el metodo comprarmascotatienda
+            Interes=Mascotas.get(rand2);
+        }
+        else{
+            // Precio aun por definir
+            String Nombre= Nombres.get(rand3);
+            if(rand2==0){
+                mascota= new Perro(Nombre,"salchicha",10000);
+            }
+            else if (rand2==1) {
+                mascota= new Gato(Nombre,10000);
+            }
+            else if(rand2==2){
+                mascota= new Pajaro(Nombre,10000);
+            }
+            else{
+                mascota= new Pez(Nombre,10000);
+            }
+        }
+    }
+
+    //Prototipo de metodos, falta corregir y mejorar
+    public String comprarMascotaTienda(Tienda tienda){
+        return (tienda.VenderMascota(mascota, Dinero));
+    }
+
+    public void VenderMascotaTienda(Tienda tienda){
+        tienda.ComprarMascota(mascota);
+
+    }
+}
