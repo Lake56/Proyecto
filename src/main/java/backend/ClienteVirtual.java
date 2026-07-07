71d13667cd78ff1ebb1b5d4bed8081d87601ccc5
Eltigre <bastiton@gmail.com>
2026-07-06 21:56:20 -0400

    Se documenta la clase ClienteVirtual en formato javadoc

--- src/main/java/backend/ClienteVirtual.java
+++ src/main/java/backend/ClienteVirtual.java
@@ -3,6 +3,11 @@ package backend;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Cliente virtual que interactuara con la tienda
+ * Obtiene un dinero inicial entre 5000 y 10000
+ * y generea aleatoriamente si este comprara o vendera a la tienda
+ */
 public class ClienteVirtual{
     Random random = new Random();
 
@@ -13,6 +18,10 @@ public class ClienteVirtual{
     private Mascota mascota;
     private int eleccion;
 
+    /**
+     * Se un nuevo cliente virtual
+     * que comprara o vendera una mascota a la tienda
+     */
     public ClienteVirtual(){
         Mascotas = new ArrayList<>();
         Nombres = new ArrayList<>();
@@ -65,22 +74,44 @@ public class ClienteVirtual{
         }
     }
 
+    /**
+     * Devuelve la eleccion del cliente
+     * si este comprara o vendera una mascota
+     * @return eleccion del cliente
+     */
     public int getEleccion(){
         return eleccion;
     }
 
+    /**
+     * El cliente comprara una mascota a la tienda
+     * @param tienda con la que este interactua
+     * @return texto resultante si la compra fue exitosa o no
+     */
     public String comprarMascotaTienda(Tienda tienda){
         return (tienda.VenderMascota(Interes, Dinero));
     }
 
+    /**
+     * Se vende una mascota a la tiena
+     * @param tienda con la que interactua el cliente
+     */
     public void VenderMascotaTienda(Tienda tienda){
         tienda.ComprarMascota(mascota);
     }
 
+    /**
+     * Obtiene el tipo de mascota que le interesa al cliente virtual
+     * @return Interes del cliente
+     */
     public TipoMascota getInteres() {
         return Interes;
     }
 
+    /**
+     * Obtiene el dinero que posee el cliente virtual
+     * * @return dinero del cliente
+     */
     public int getDinero() {
         return Dinero;
     }
